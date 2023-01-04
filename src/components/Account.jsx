22bfae88f4c1d0833f83fb92a708a76a372470a3
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-01-04 11:45:01 +0530

    added no-cors

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -121,6 +121,7 @@ const Account = ({ Header, Footer, Loader, Error }) => {
     }
 
     const res = await fetch(`${BACKEND_URL}/api/v1/users/updateMe`, {
+      mode: "no-cors",
       method: "PATCH",
       credentials: "include",
       headers: {
