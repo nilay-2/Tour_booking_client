4f5af0a4b86a71f5b67d9a66eb84328c4d16e532
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-01-07 11:51:12 +0530

    remove tourData from logic

--- src/components/MyReviews.jsx
+++ src/components/MyReviews.jsx
@@ -148,7 +148,7 @@ const MyReviews = ({ Header, Footer, Loader }) => {
       />
       <Header />
       <main className="main">
-        {isLoading && tourData.length === 0 ? (
+        {isLoading ? (
           <Loader />
         ) : (
           <>
