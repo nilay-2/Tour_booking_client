20cb15f450e0abb393aecde026cf0ea4786e5d12
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-01-24 20:06:03 +0530

    remove logs from account.jsx

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -164,11 +164,11 @@ const Account = ({ Loader, Error, ImageLoader }) => {
     }
     return;
   };
-  console.log(data?.photo);
+  // console.log(data?.photo);
   // delete user profile image
   const deleteProfilePic = async (e) => {
     setBtnProcessing(true);
-    console.log("imageURL for deleting", data?.photo);
+    // console.log("imageURL for deleting", data?.photo);
     const imageRef = ref(storage, data?.photo);
     deleteObject(imageRef).then(() => {
       console.log("image delete successfully!");
